508ae325417d0e193bb1987410cb45dbcd2d97ad
faridirsyad <101861509+faridirsyad@users.noreply.github.com>
2026-06-24 13:39:07 +0700

    Eloquent ORM

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,33 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Almaas',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, repellendus rem voluptate numquam, assumenda commodi cumque itaque eius iusto nihil, ullam nemo perspiciatis? Dolorum ab molestias soluta ipsum nulla nisi.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Irsyad',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta dolore a est, similique soluta facere doloribus quis laudantium consectetur et eveniet ducimus iusto magni ipsa harum nobis eos quos!'
-            ]
-        ];
-    }
-
-    public static function find($slug)
-    {
-        // function arrow
-        return Arr::first(static::all(), fn($p) => $p['slug'] == $slug) ?? abort(404);
-    }
+    // dibolehkan mengisi pada tinker
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+    // dijaga agar tidak mengisi data 
+    // protected $guarded = [];
+    // protected $table = 'blog_posts'; jika nama table berbeda dengan nama class
 }
